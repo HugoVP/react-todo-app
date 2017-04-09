c4b5fd2f366922a64b72bd81a23cf5e480654ed2
Hugo <hugo.vazquez.paleo@gmail.com>
2017-04-09 00:54:26 -0500

    Move Firebase credentials to ENV variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,5 +1,12 @@
 import firebase from 'firebase';
-import config   from 'app/firebase/.config';
+const config = {
+  apiKey            : process.env.API_KEY,
+  authDomain        : process.env.AUTH_DOMAIN,
+  databaseURL       : process.env.DATABASE_URL,
+  projectId         : process.env.PROJECT_ID,
+  storageBucket     : process.env.STORAGE_BUCKET,
+  messagingSenderId : process.env.MESSAGING_SENDER_ID
+};
 try {
   firebase.initializeApp(config);
 } catch (err) {
